1fffbbde2693500033e84c6ee6ff1f06ce4c10ab
nverdavtyan <nverdavtyan1994@gmail.com>
2021-11-01 16:20:41 +0100

    deploy my react app to github pages

---
+++ src/pages/About.js
@@ -0,0 +1,26 @@
+import Navigation from "../components/Navigation";
+
+
+const About = () => {
+    return (
+        <div>
+         <Navigation/>
+            <h1> A Propos</h1>
+            <br/>
+            <p>     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                 Obcaecati quod recusandae eveniet necessitatibus. Animi qui 
+                 voluptatem, odio error natus, nobis distinctio ab dolores facilis accusamus, 
+                 suscipit quos? Commodi praesentium adipisci 
+                doloribus itaque, veniam aliquam dolorem eaque. Aliquid cupiditate obcaecati tenetur. </p>
+                <br/>
+
+                <p>     Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                 Obcaecati quod recusandae eveniet necessitatibus. Animi qui 
+                 voluptatem, odio error natus, nobis distinctio ab dolores facilis accusamus, 
+                 suscipit quos? Commodi praesentium adipisci 
+                doloribus itaque, veniam aliquam dolorem eaque. Aliquid cupiditate obcaecati tenetur. </p>
+        </div>
+    );
+};
+
+export default About;
